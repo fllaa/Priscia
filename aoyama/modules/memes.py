e643da5814cedec91a2d0880f36bdea09f2ce662
Fallavall <fallahandyprakasa26@gmail.com>
2020-09-10 06:26:35 +0700

    memes.py: fix iwi

--- aoyama/modules/memes.py
+++ aoyama/modules/memes.py
@@ -432,9 +432,9 @@ def iwi(update, context):
             r"¯\_༼ ಥ ‿ ಥ ༽_/¯",
             "乁( ⁰͡  Ĺ̯ ⁰͡ ) ㄏ",
         ]
-        reply_text = sub(r"(a|i|u|e|o)", "i", message)
-        reply_text = sub(r"(A|I|U|E|O)", "I", reply_text)
-        reply_text = sub(r"\!+", " " + random.choice(ficis), reply_text)
+        reply_text = re.sub(r"(a|i|u|e|o)", "i", message)
+        reply_text = re.sub(r"(A|I|U|E|O)", "I", reply_text)
+        reply_text = re.sub(r"\!+", " " + random.choice(ficis), reply_text)
         reply_text += " " + random.choice(ficis)
         message.reply_to_message.reply_text(reply_text)
 
