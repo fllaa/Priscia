8fa1838d5931df7a49d52da05b9fa84e8a6a7e56
Fallavall <37959363+Fallavall@users.noreply.github.com>
2020-09-23 14:49:35 +0700

    change variable

--- aoyama/modules/memes.py
+++ aoyama/modules/memes.py
@@ -449,11 +449,11 @@ def mock(update, context):
         for charac in message.reply_to_message.text:
             if charac.isalpha() and random.randint(0, 1):
                 to_app = charac.upper() if charac.islower() else charac.lower()
-                reply_text = reply_text.append(to_app)
+                texty = reply_text.append(to_app)
             else:
-                reply_text = reply_text.append(charac)
+                texty = reply_text.append(charac)
                 
-        message.reply_to_message.reply_text(reply_text)
+        message.reply_to_message.reply_text(texty)
 
 
 @run_async
