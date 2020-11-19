4e9d3b7d7ada1053e1c6706371386bc136564d62
Fallavall <fallahandyprakasa26@gmail.com>
2020-11-20 01:18:14 +0700

    chat_status.py: whitelist @GroupAnonymousBot update .gitignore

--- priscia/modules/helper_funcs/chat_status.py
+++ priscia/modules/helper_funcs/chat_status.py
@@ -17,7 +17,7 @@ def is_user_ban_protected(
         chat.type == "private"
         or user_id in SUDO_USERS
         or user_id in WHITELIST_USERS
-        or user_id == int(777000)
+        or user_id in (777000, 1087968824)
         or chat.all_members_are_administrators
     ):
         return True
@@ -32,7 +32,7 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
     if (
         chat.type == "private"
         or user_id in SUDO_USERS
-        or user_id == int(777000)
+        or user_id in (777000, 1087968824)
         or chat.all_members_are_administrators
     ):
         return True
