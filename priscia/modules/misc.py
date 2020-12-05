6d4ab250721c1240270b660f9b59b272480a59ad
AkaruiAikara <fallahandyprakasa26@gmail.com>
2020-12-05 09:52:51 +0000

    pylint autofix
    Signed-off-by: AkaruiAikara <fallahandyprakasa26@gmail.com>

--- priscia/modules/misc.py
+++ priscia/modules/misc.py
@@ -585,35 +585,76 @@ def covid(update, context):
         message.reply_photo(photo=flag, caption=text, parse_mode=ParseMode.MARKDOWN)
     except BaseException:
         message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
-        
-        
+
+
 @run_async
 @typing_action
 def app(update, context):
     try:
         args = context.args
         app_name = " ".join(args)
-        remove_space = app_name.split(' ')
-        final_name = '+'.join(remove_space)
-        page = get("https://play.google.com/store/search?q="+final_name+"&c=apps")
-        soup = BeautifulSoup(page.content,'lxml', from_encoding='utf-8')
-        results = soup.findAll("div","ZmHEEd")
-        app_name = results[0].findNext('div', 'Vpfmgd').findNext('div', 'WsMG1c nnK0zc').text
-        app_dev = results[0].findNext('div', 'Vpfmgd').findNext('div', 'KoLSrc').text
-        app_dev_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('a', 'mnKHRc')['href']
-        app_rating = results[0].findNext('div', 'Vpfmgd').findNext('div', 'pf5lIe').find('div')['aria-label']
-        app_link = "https://play.google.com"+results[0].findNext('div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
-        app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
-        app_details = "<a href='"+app_icon+"'>📲&#8203;</a>"
-        app_details += " <b>"+app_name+"</b>"
-        app_details += "\n\n<code>Developer :</code> <a href='"+app_dev_link+"'>"+app_dev+"</a>"
-        app_details += "\n<code>Rating :</code> "+app_rating.replace("Rated ", "⭐ ").replace(" out of ", "/").replace(" stars", "", 1).replace(" stars", "⭐ ").replace("five", "5")
-        app_details += "\n<code>Features :</code> <a href='"+app_link+"'>View in Play Store</a>"
-        update.effective_message.reply_text(app_details, link_preview = True, parse_mode = 'HTML')
+        remove_space = app_name.split(" ")
+        final_name = "+".join(remove_space)
+        page = get("https://play.google.com/store/search?q=" + final_name + "&c=apps")
+        soup = BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
+        results = soup.findAll("div", "ZmHEEd")
+        app_name = (
+            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
+        )
+        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
+        app_dev_link = (
+            "https://play.google.com"
+            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
+        )
+        app_rating = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "pf5lIe")
+            .find("div")["aria-label"]
+        )
+        app_link = (
+            "https://play.google.com"
+            + results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "vU6FJ p63iDd")
+            .a["href"]
+        )
+        app_icon = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "uzcko")
+            .img["data-src"]
+        )
+        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
+        app_details += " <b>" + app_name + "</b>"
+        app_details += (
+            "\n\n<code>Developer :</code> <a href='"
+            + app_dev_link
+            + "'>"
+            + app_dev
+            + "</a>"
+        )
+        app_details += "\n<code>Rating :</code> " + app_rating.replace(
+            "Rated ", "⭐ "
+        ).replace(" out of ", "/").replace(" stars", "", 1).replace(
+            " stars", "⭐ "
+        ).replace(
+            "five", "5"
+        )
+        app_details += (
+            "\n<code>Features :</code> <a href='"
+            + app_link
+            + "'>View in Play Store</a>"
+        )
+        update.effective_message.reply_text(
+            app_details, link_preview=True, parse_mode="HTML"
+        )
     except IndexError:
-        update.effective_message.reply_text("No result found in search. Please enter **Valid app name**")
+        update.effective_message.reply_text(
+            "No result found in search. Please enter **Valid app name**"
+        )
     except Exception as err:
-        update.effective_message.reply_text("Exception Occured:- "+str(err))
+        update.effective_message.reply_text("Exception Occured:- " + str(err))
 
 
 @run_async
